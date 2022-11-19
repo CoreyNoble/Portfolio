0e529cd1053ed694f0b024b0172f27d0ffbdba6c
Corey Noble <coreynoble@hotmail.ca>
2022-11-18 20:58:39 -0500

    Design Tokens - Replacing 'spacing' with 'sizing', add 'spacing' as css classes that control margin + padding. Beginning to look into toggling theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.scss';
-import Navigation from './components/Navigation/Navigation';
-import Hero from './components/Hero/Hero';
-import Portfolio from './components/Portfolio/Portfolio';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
+import { useState } from "react";
+import "./App.scss";
+import "../src/utils/spacing.scss";
+import Navigation from "./components/Navigation/Navigation";
+import Hero from "./components/Hero/Hero";
+import Portfolio from "./components/Portfolio/Portfolio";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import { Button } from "./components/Button/Button";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
   return (
     <div className="App">
+      <Button className="m-b-l" onClick={toggleDarkMode}>
+        Toggle Theme ({isDarkMode ? "Light Mode" : "Dark Mode"})
+      </Button>
+
       <Navigation />
       <Hero />
       <Portfolio />
