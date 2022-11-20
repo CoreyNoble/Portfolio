44d1980ac5cb7dd5653115f89c530d1cea2a1850
Corey Noble <coreynoble@hotmail.ca>
2022-11-20 17:58:59 -0500

    Skeleton - Initial pass at mockup for the Portfolio section

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,21 +1,220 @@
 import Section from "../Section/Section";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faUpRightFromSquare,
+  faCircleInfo,
+} from "@fortawesome/free-solid-svg-icons";
+import portfolio_motusbank from "../../assets/img/portfolio/motusbank.png";
+import portfolio_pc_financial from "../../assets/img/portfolio/pc-financial.png";
 import "./Portfolio.scss";
 
 function Portfolio() {
   return (
     <Section title="Portfolio" inverse={true} spacing={true}>
-      <div className="portfolio">
-        <h1>
-          Hi! 👋
-          <br />
-          I'm Corey Noble
-        </h1>
-        <p className="h2">
-          A front-end engineer based in Ontario,&nbsp;Canada&nbsp;🇨🇦
-        </p>
+      <div className="portfolio__item">
+        <h3 className="portfolio__item-title">motusbank</h3>
+        <div className="portfolio__item-content">
+          <img src={portfolio_motusbank} />
+          <div className="portfolio__item-content-external-link">
+            <a
+              href="https://github.com/CoreyNoble"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="View my GitHub Profile"
+              className="button icon"
+            >
+              <FontAwesomeIcon icon={faUpRightFromSquare} />
+            </a>
+          </div>
+          <div className="portfolio__item-content-info">
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faCircleInfo} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+            </ul>
+          </div>{" "}
+          <div className="portfolio__item-content-skills">
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="hero__animation">
-        <img src="https://via.placeholder.com/700x600.png" />
+      <div className="portfolio__item">
+        <h3 className="portfolio__item-title">PC Financial</h3>
+        <div className="portfolio__item-content">
+          <img src={portfolio_pc_financial} />
+          <div className="portfolio__item-content-external-link">
+            <a
+              href="https://github.com/CoreyNoble"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="View my GitHub Profile"
+              className="button icon"
+            >
+              <FontAwesomeIcon icon={faUpRightFromSquare} />
+            </a>
+          </div>
+          <div className="portfolio__item-content-info">
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faCircleInfo} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="portfolio__item-content-skills">
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/CoreyNoble"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="View my GitHub Profile"
+                  className="button icon"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </Section>
   );
